6c333fc6989da76bb367591187ac570ef38df57c
doquolo <doquolo123@outlook.com>
2024-07-07 00:32:40 +0700

    adding hash capabilities

--- main.py
+++ main.py
@@ -2,12 +2,16 @@ from flask import Flask, render_template, request
 import firebase_admin
 from firebase_admin import db, firestore
 import json
+import argon2
 
 # init firebase
 databaseURL = "https://waterflow-583e8-default-rtdb.asia-southeast1.firebasedatabase.app/"
 cred_obj = firebase_admin.credentials.Certificate("creds.json")
 default_app = firebase_admin.initialize_app(cred_obj, {"databaseURL": databaseURL})
 firestore = firestore.client()
+
+# init hasher
+hasher = argon2.PasswordHasher()
 
 # init flask
 app = Flask(__name__)
@@ -23,10 +27,11 @@ def getUserData():
     try:
         user = firestore.collection("users").document(str(username)).get()
         user = user.to_dict()
-        print(user)
-        if (user == None or user["password"] != str(password)):
+        if (user != None):
+            isPasswordCorrect = hasher.verify(user["password"], password)
+        elif (user == None or not isPasswordCorrect):
             return {"state": "authenticate"}
-        elif (user["password"] == str(password)):
+        elif (isPasswordCorrect):
             device_list = []
             for device in user["devices"]:
                 device_list.append(device)
